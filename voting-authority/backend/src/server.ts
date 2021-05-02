00d5eaf0e8723a27bf1368d8afab4405d7d40ceb
Roger Staubli <roger.staubli@uzh.ch>
2021-05-02 05:30:59 +0200

    add basic auth to voting authority

--- voting-authority/backend/src/server.ts
+++ voting-authority/backend/src/server.ts
@@ -20,6 +20,30 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(requestLogger)
 server.use(responseLogger)
+server.use((req, res, next) => {
+
+  // -----------------------------------------------------------------------
+  // authentication middleware
+
+  const auth = {login: 'test', password: 'test'} // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+  res.status(401).send('Authentication required.') // custom message
+
+  // -----------------------------------------------------------------------
+
+})
 server.use(
   cors({
     origin: [
